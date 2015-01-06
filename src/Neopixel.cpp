38b029e9bc5ee0cfcebf962a1cc4982dbdfdb774
ADELF <adelle.lin@gmail.com>
2015-01-06 08:26:40 -0500

    no message

---
+++ src/Neopixel.cpp
@@ -0,0 +1,9 @@
+//
+//  Neopixel.cpp
+//  Meud_audioToy
+//
+//  Created by Adelle on 21/12/14.
+//
+//
+
+#include "Neopixel.h"
---
+++ src/Neopixel.h
@@ -0,0 +1,14 @@
+//
+//  Neopixel.h
+//  Meud_audioToy
+//
+//  Created by Adelle on 21/12/14.
+//
+//
+
+#ifndef __Meud_audioToy__Neopixel__
+#define __Meud_audioToy__Neopixel__
+
+#include <iostream>
+
+#endif /* defined(__Meud_audioToy__Neopixel__) */
